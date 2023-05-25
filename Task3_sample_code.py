19fbc5c57cc161294b6aaaad7ebf8309a29aebd5
Judith Zhao <judithzhao@outlook.com>
2023-05-25 16:33:29 -0700

    Task3_Testing

--- Task3_sample_code.py
+++ Task3_sample_code.py
@@ -7,12 +7,25 @@ import os
 async def query(prompt):
     output = (await lmql.run(prompt, output_writer=lmql.stream("RESPONSE")))
     return output
+def match_test(prompt, answer, file_name):
+    questions = data["stuff"]
+
+    for q in questions:
+      for prompt_in_Json in q["prompt"]:
+        if prompt_in_Json in prompt:
+            if answer == q["answer"]:
+                return 1
+    # If no matches were found, return 0
+    return 0
+
 
 if __name__ == "__main__":
     json_name = '/home/jzhao5793/proj/modulus-magnus-linguae/sample_answers_Task3.json'
     with open(json_name, 'r') as f:
         data = json.load(f)
     stuffs=data["stuff"]
+    a = 0
+    b = 0
     for q in stuffs:
         for codes in q["code"]:
             response = asyncio.run(query(codes))
@@ -23,17 +36,4 @@ if __name__ == "__main__":
             print(prompt_from_Output, answer_from_Output)
     accu_rate = [a/b, b]
     print(accu_rate)
-
-def match_test(prompt, answer, file_name):
-    questions = data["stuff"]
-
-    for q in questions:
-      for prompt_in_Json in q["prompt"]:
-        if prompt_in_Json in prompt:
-            print("in")
-            if answer == q["answer"]:
-                return 1
-    # If no matches were found, return 0
-    return 0
-
 print(match_test(prompt_from_Output, answer_from_Output, json_name))
