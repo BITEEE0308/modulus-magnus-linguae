71bb4aadd9a6d793923d2724ed9ad94551d57201
Judith Zhao <judithzhao@outlook.com>
2023-05-25 16:27:37 -0700

    Task3_Testing

--- Task3_sample_code.py
+++ Task3_sample_code.py
@@ -1,25 +1,39 @@
+import lmql
+import asyncio
 import json
+import os
 
-list_object = [
-    "Q: Nilus fluvi~ est. A:",
-    "us"
-]
 
-file_name = 'sample_answers_Task3.json'
+async def query(prompt):
+    output = (await lmql.run(prompt, output_writer=lmql.stream("RESPONSE")))
+    return output
 
-def match_test(list_object, file_name):
-    # Open the JSON file
-    with open(file_name, 'r') as f:
+if __name__ == "__main__":
+    json_name = '/home/jzhao5793/proj/modulus-magnus-linguae/sample_answers_Task3.json'
+    with open(json_name, 'r') as f:
         data = json.load(f)
+    stuffs=data["stuff"]
+    for q in stuffs:
+        for codes in q["code"]:
+            response = asyncio.run(query(codes))
+            prompt_from_Output = response[0].prompt
+            answer_from_Output = response[0].variables['ANSWER']
+            a += match_test(prompt_from_Output, answer_from_Output, json_name)
+            b += 1
+            print(prompt_from_Output, answer_from_Output)
+    accu_rate = [a/b, b]
+    print(accu_rate)
 
-    questions = data["questions"]
+def match_test(prompt, answer, file_name):
+    questions = data["stuff"]
 
     for q in questions:
-        if list_object[0] in q["prompts"]:
-            # If the second object in list_object matches the "answer"
-            if list_object[1] == q["answer"]:
+      for prompt_in_Json in q["prompt"]:
+        if prompt_in_Json in prompt:
+            print("in")
+            if answer == q["answer"]:
                 return 1
-
     # If no matches were found, return 0
     return 0
-print(match_test(list_object, file_name))
+
+print(match_test(prompt_from_Output, answer_from_Output, json_name))
