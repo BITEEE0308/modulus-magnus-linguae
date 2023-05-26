90ef1a89f3ece1f2302ec4acd4606e58837bdd0f
Judith Zhao <judithzhao@outlook.com>
2023-05-26 15:15:32 -0700

    testing

--- Task3_sample_code.py
+++ Task3_sample_code.py
@@ -7,27 +7,29 @@ import sys
 async def query(prompt):
     output = (await lmql.run(prompt, output_writer=lmql.stream("RESPONSE")))
     return output
+
 def match_test(code, answer, file_name):
     codes = data["codes"]
     for c in codes:
-        if answer == c["answer"]:
-            return 1
+        if code == c["code"]:
+            if answer == c["answer"]:
+                return 1
     return 0
 
 def main(filename):
-    with open(filename, 'r') as f:
-        data = json.load(f)
-
     codes=data["codes"]
     a = 0
     b = 0
     for c in codes:
-        response = asyncio.run(query(c))
-        answer_from_Output = response[0].variables['ANSWER']
-        a += match_test(c, answer_from_Output, data)
-        b += 1
+            response = asyncio.run(query(c["code"]))
+            answer_from_Output = response[0].variables['ANSWER']
+            a += match_test(c, answer_from_Output, data)
+            b += 1
     accu_rate = a/b
     return accu_rate
 
 if __name__ == "__main__":
-    main(sys.argv[0])
+    print(len(sys.argv))
+    with open(sys.argv[1], 'r') as f:
+        data = json.load(f)
+    main(data)
