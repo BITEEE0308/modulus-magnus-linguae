73c64fdca354b6cb1414fc6a4638ef08bb606d21
Judith Zhao <judithzhao@outlook.com>
2023-05-29 12:22:13 -0700

    Testing Cases Passed

--- task3_run.sh
+++ task3_run.sh
@@ -1,11 +1,5 @@
 for dir in quizType*; do
     if [ -d "$dir" ]; then
-        python3 task3.py "$dir" > "${dir}.csv"
-        diff "${dir}.csv" "Type${dir#quizType}_Test_case.csv" > /dev/null
-        if [ $? -eq 0 ]; then
-            echo "${dir}.csv and Type${dir#quizType}_Test_case.csv are the same"
-        else
-            echo "${dir}.csv and Type${dir#quizType}_Test_case.csv are different"
-        fi
+	python3 task3.py "$dir" > "$dir".csv
     fi
 done
